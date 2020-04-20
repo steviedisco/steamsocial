3806e7d10c8d6ce76e1094448584c8af8cab0e8e
Steve Driscoll <steved@intuitivesystems.co.uk>
2020-04-20 11:46:08 +0100

    sett ing up a basic preact app from scratch, eslint, webpack configs etc

---
+++ steamroom-ui/src/app.tsx
@@ -0,0 +1,27 @@
+import { h, Component } from "preact";
+
+export interface AppProps {
+  name: string;
+}
+
+interface AppState {
+  name: string;
+}
+
+export class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { name: props.name };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      var state = this.state;
+      this.state = { name: "Preact's componentDidMount worked as expected" };
+      this.setState(state);
+    }, 2000);
+  }
+  render(props: AppProps, state: AppState) {
+    return <h1>props: {props.name} state: {state.name}</h1>;
+  }
+}
---
+++ steamroom-ui/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const fs = require("fs");
+// const HtmlWebpackPlugin = await import("html-webpack-plugin");
+
+// App directory
+const appDirectory = fs.realpathSync(process.cwd());
+
+// Gets absolute path of file within app directory
+const resolveAppPath = (relativePath) =>
+  path.resolve(appDirectory, relativePath);
+
+// Host
+const host = process.env.HOST || "localhost";
+
+// Required for babel-preset-react-app
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  // Environment mode
+  mode: "development",
+  entry: "./src/bootstrap.tsx",
+  output: {
+    filename: "./public/bundle.js",
+  },
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "eval",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      // Handle .ts and .tsx file via ts-loader.
+      { test: /\.tsx?$/, loader: "ts-loader" },
+    ],
+  },
+  devServer: {
+    // Serve index.html as the base
+    contentBase: resolveAppPath("public"),
+    // Enable compression
+    compress: true,
+    // Enable hot reloading
+    hot: true,
+    host,
+    port: 3000,
+    // Public path is root of content base
+    publicPath: "/",
+  },
+};
